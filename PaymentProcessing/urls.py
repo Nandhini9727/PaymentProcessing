82b6812c173bb2c8a53262aa014c9a1aaefea76a
Nandhini Saravanan <nandhu.sara97@gmail.com>
2024-04-27 14:25:29 -0700

    fourth commit

--- PaymentProcessing/urls.py
+++ PaymentProcessing/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for payment_processing_main project.
+URL configuration for PaymentProcessing project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
